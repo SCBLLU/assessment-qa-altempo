62abeb92b30fd6184effa199b90b4ceda5557bf5
Rafael Ernesto <scbllu.200@gmail.com>
2026-02-16 22:39:56 -0600

    Actualizar configuración de Playwright generar rastreos en caso de fallos

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,5 +11,11 @@ export default defineConfig({
     viewport: { width: 1280, height: 720 },
     // Ignorar errores de certificado SSL si los hubiera
     ignoreHTTPSErrors: true,
+    // Graba video si falla
+    video: "on-first-retry",
+    // Genera un archivo de rastreo si falla
+    trace: "retain-on-failure",
+    // Toma una foto exacta del error
+    screenshot: "only-on-failure",
   },
 });
